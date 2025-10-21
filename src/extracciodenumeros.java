2dde7f304b709002dc8496242a23f0b614318020
daw1 <emmachoquesandoval@elpuig.xeill.net>
2025-10-21 16:08:08 +0200

    Comienzo nuevo ejercicio

---
+++ src/extracciodenumeros.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class extracciodenumeros {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+
+        //El teu codi aqui
+        String palabra = sc.nextLine();
+
+//        p1 = p1.substring(1);
+//
+//        int l = p2.length();
+//        p2 = p2.substring(0,l-2);
+//        int n1 = Integer.valueOf(p1);
+//        int n2 = Integer.valueOf(p2);
+//        System.out.println(n1+n2);
+
+    }
+}
