7463f9c579890284a1ac30495e06a6b77951ae92
patomayi <patomayi@hotmail.com>
2025-12-11 18:09:30 +0100

    La saltarina mal

--- src/lasaltarina.java
+++ src/lasaltarina.java
@@ -7,5 +7,24 @@ public class lasaltarina {
         sc.useLocale(Locale.ENGLISH);
 
         //El teu codi aqui
+        int longitud = sc.nextInt();
+        int energia = sc.nextInt();
+        int[] camino = new int[longitud];
+        int caminarI = 0;
+        int caminarD = 0;
+        int j = 0;
+        //Llenado:
+        for(int i = 0; i < longitud; i++){
+            camino[i] = sc.nextInt();
+        }
+
+        while(camino[j]!=-1){
+            if(camino[j]==0){
+                caminarI++;
+            }else{
+                caminarI += camino[j]*2;
+            }
+        }
+        for(int elem : camino) System.out.println(elem);
     }
 }
