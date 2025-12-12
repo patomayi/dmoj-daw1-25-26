b9e5c615598394f93b501295f85d27aa06597c47
patomayi <patomayi@hotmail.com>
2025-12-12 10:06:48 +0100

    15 En proceso

--- src/lasaltarina.java
+++ src/lasaltarina.java
@@ -10,21 +10,40 @@ public class lasaltarina {
         int longitud = sc.nextInt();
         int energia = sc.nextInt();
         int[] camino = new int[longitud];
-        int caminarI = 0;
-        int caminarD = 0;
-        int j = 0;
+        int pasosI = 0;
+        int pasosD = 0;
+        int energiaI = 0;
+        int energiaD = 0;
+
         //Llenado:
         for(int i = 0; i < longitud; i++){
             camino[i] = sc.nextInt();
         }
 
-        while(camino[j]!=-1){
-            if(camino[j]==0){
-                caminarI++;
-            }else{
-                caminarI += camino[j]*2;
+        for(int j = 0; j < longitud / 2; j++){
+            if (camino[j] == -1){
+                break;
+            }else if (camino[j] == 0){
+                pasosI++;
+                energiaI++;
+            }else {
+                pasosI += camino[j];
+                energiaI += camino[j]*2;
             }
         }
-        for(int elem : camino) System.out.println(elem);
+
+        for(int k = longitud-1; k >= longitud / 2; k--){
+            if (camino[k] == -1){
+                break;
+            }else if (camino[k] == 0){
+                pasosD++;
+                energiaD++;
+            }else {
+                pasosD += camino[k];
+                energiaD += camino[k]*2;
+            }
+        }
+        System.out.println("Pel cantó dret, s'arriba al tresor en "+ pasosD +" passos consumint "+energiaD+" unitats d'energia.");
+        System.out.println("Pel cantó esquerra, s'arriba al tresor en "+ pasosI +" passos consumint "+energiaI+" unitats d'energia.");
     }
 }
