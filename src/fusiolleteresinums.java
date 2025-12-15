2ce5193b77bcd03535417126607229f4e6bbae2d
patomayi <patomayi@hotmail.com>
2025-12-15 12:10:32 +0100

    Falta acabar la saltarina

--- src/fusiolleteresinums.java
+++ src/fusiolleteresinums.java
@@ -7,56 +7,57 @@ public class fusiolleteresinums {
         sc.useLocale(Locale.ENGLISH);
 
         // Leer tamaño de las listas
-        int nt1 = sc.nextInt();
-        char[] lista1 = new char[nt1];
-        for (int i = 0; i < nt1; i++) {
+        int tam1 = sc.nextInt();
+        char[] lista1 = new char[tam1];
+        //Aqui lee caracter por caracter y lo acomoda en el array
+        for (int i = 0; i < tam1; i++) {
             lista1[i] = sc.next().charAt(0);
         }
 
-        int nt2 = sc.nextInt();
-        char[] lista2 = new char[nt2];
-        for (int i = 0; i < nt2; i++) {
+        int tam2 = sc.nextInt();
+        char[] lista2 = new char[tam2];
+        for (int i = 0; i < tam2; i++) {
             lista2[i] = sc.next().charAt(0);
         }
 
         // Separar números y letras en orden de llegada
-        char[] numeros = new char[nt1 + nt2];
-        char[] letras = new char[nt1 + nt2];
-        int posNum = 0;
-        int posLet = 0;
+        char[] numeros = new char[tam1 + tam2];
+        char[] letras = new char[tam1 + tam2];
+        int posicionNumero = 0;
+        int posicionLetra = 0;
 
-        int max = nt1 > nt2 ? nt1 : nt2;
+        int max = tam1 > tam2 ? tam1 : tam2;
         for (int i = 0; i < max; i++) {
-            if (i < nt1) {
+            if (i < tam1) {
                 if (lista1[i] >= '0' && lista1[i] <= '9') {
-                    numeros[posNum] = lista1[i];
-                    posNum++;
+                    numeros[posicionNumero] = lista1[i];
+                    posicionNumero++;
                 } else {
-                    letras[posLet] = lista1[i];
-                    posLet++;
+                    letras[posicionLetra] = lista1[i];
+                    posicionLetra++;
                 }
             }
-            if (i < nt2) {
+            if (i < tam2) {
                 if (lista2[i] >= '0' && lista2[i] <= '9') {
-                    numeros[posNum] = lista2[i];
-                    posNum++;
+                    numeros[posicionNumero] = lista2[i];
+                    posicionNumero++;
                 } else {
-                    letras[posLet] = lista2[i];
-                    posLet++;
+                    letras[posicionLetra] = lista2[i];
+                    posicionLetra++;
                 }
             }
         }
 
         // Fusionar intercalando
-        char[] resultado = new char[posNum + posLet];
+        char[] resultado = new char[posicionNumero + posicionLetra];
         int i = 0, j = 0, k = 0;
-        while (i < posNum || j < posLet) {
-            if (i < posNum) {
+        while (i < posicionNumero || j < posicionLetra) {
+            if (i < posicionNumero) {
                 resultado[k] = numeros[i];
                 i++;
                 k++;
             }
-            if (j < posLet) {
+            if (j < posicionLetra) {
                 resultado[k] = letras[j];
                 j++;
                 k++;
--- src/reaccioencadena.java
+++ src/reaccioencadena.java
@@ -8,23 +8,27 @@ public class reaccioencadena {
 
         int n = sc.nextInt();
         int[] alcances = new int[n];
-        
+
         for (int i = 0; i < n; i++) {
             alcances[i] = sc.nextInt();
         }
-        
+
         // La bomba 0 siempre explota
-        int alcanceMaximo = alcances[0];
+        int alcanceMaximo = 0 + alcances[0];
         int ultimaBomba = 0;
-        
+
         // Recorremos todas las bombas para ver cuáles explotan
         for (int i = 1; i < n; i++) {
             if (i <= alcanceMaximo) {
                 ultimaBomba = i;
-                alcanceMaximo = Math.max(alcanceMaximo, i + alcances[i]);
+                int nuevoAlcance = i + alcances[i];
+                if (nuevoAlcance > alcanceMaximo) {
+                    alcanceMaximo = nuevoAlcance;  // actualizamos solo si es mayor
+                }
             }
         }
-        
-        System.out.println(ultimaBomba);
+
+        // Imprimir la posición (empezando en 1)
+        System.out.println(ultimaBomba + 1);
     }
 }
