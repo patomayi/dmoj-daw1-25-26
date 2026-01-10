742d6f4627ac9513256ba9c5a5f7fd9c032eab4c
patomayi <patomayi@hotmail.com>
2026-01-10 23:01:38 +0100

    Acabado bomberman2.java

--- src/encontrarFilaColumna.java
+++ src/encontrarFilaColumna.java
@@ -1,7 +1,7 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-public class bomberman2 {
+public class encontrarFilaColumna {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
