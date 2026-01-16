6e184a94a63b57bb4546f6952830dacb1f89459c
patomayi <patomayi@hotmail.com>
2026-01-16 14:34:42 +0100

    Empezando ejercicio paellaMetodo.java

---
+++ src/paellaMetodo.java
@@ -0,0 +1,25 @@
+/*
+Paella
+Suponiendo que una paellaMetodo se puede cocinar exclusivamente con arroz y gambas (que es mucho suponer),
+y que por cada 4 personas se utiliza medio kilo de arroz y un cuarto de kilo de gambas,
+escribe un programa que lea el número de comensales, el precio por kilo de arroz y el precio por kilo de kilo gambas,
+y muestre las cantidades necesarias y el coste de cada ingrediente, y el coste total de la paellaMetodo.
+
+cada 4 personas 0.5 kilo de arroz 0.25 k de gambas
+leer comensales,
+leer 1k arroz y
+leer 1k gambas
+
+coste de paellaMetodo
+ */
+import java.util.Locale;
+import java.util.Scanner;
+
+public class paellaMetodo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+        //El teu codi aqui
+
+    }
+}
