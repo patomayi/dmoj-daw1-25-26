f710eaad71b7445e94f14016a5179c2349705014
daw1 <emmachoquesandoval@elpuig.xeill.net>
2026-02-23 17:32:34 +0100

    Actual

--- src/arraylistinsertv2.java
+++ src/arraylistinsertv2.java
@@ -1,22 +1,20 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 public class arraylistinsertv2 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
-        List<Integer> llista = new ArrayList<>();
+        List<Integer> llista = new LinkedList<>();
         //posibles preguntas de examen escoger entre ArrayList o LinkedList
 
         while(true){
             int num = sc.nextByte();
             if(num == -1)break;
-            int pos = sc.nextInt();
-            if(pos>0){
 
-            }
+            int pos = sc.nextInt();
+            if(pos>0) llista.addLast(num);//llista.add(num)
+            else llista.addFirst(num);//llista.add(0,num)
         }
+        llista.stream().forEach(Integer x -> System.out.print(x+" "));
     }
 }
